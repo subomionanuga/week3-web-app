3fe5a3beaba7463366ec3f199126e7ab020e7a50
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2018-12-10 16:34:59 +0000

    First commit - Sinatra and Shotgun gems installed

---
+++ app.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+
+get '/' do
+  "hello world"
+end
+
+get '/secret' do
+  "This is our private server. GET, LOST"
+end
+
+get '/sushi' do
+  "I love, Salmon"
+end
+
+get '/lobster' do
+  "Steamed Lobster with Garlic Bitter"
+end
