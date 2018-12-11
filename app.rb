98cd136c214d9faace37845c8c8a55a50a228a06
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2018-12-11 16:37:21 +0000

    Sinatra - Introducing params

--- app.rb
+++ app.rb
@@ -16,7 +16,13 @@ get '/lobster' do
   "Steamed Lobster with Garlic Butter"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
